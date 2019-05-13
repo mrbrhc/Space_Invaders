6ff8ed8bd798c63c2589bce95762133d8675c51a
Mason Breece <mrbrhc@mail.missouri.edu>
2019-05-13 18:36:14 -0500

    Changed 2nd shoot button to W from E

--- main.cpp
+++ main.cpp
@@ -111,7 +111,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 	case GLFW_KEY_SPACE:
 		if (action == GLFW_RELEASE) fire_pressed = true;
 		break;
-	case GLFW_KEY_E:
+	case GLFW_KEY_W:
 		if (action == GLFW_RELEASE) fire_pressed = true;
 		break;
 	case GLFW_KEY_ENTER:
